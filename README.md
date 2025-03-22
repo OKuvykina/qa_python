fe1ada44dd4ec61bc087755a8735d22e3b21ee2d
OLGA KUVYKINA <Olga.Kuvykina2016@yandex.ru>
2025-03-22 23:15:35 +0300

    Sprint_4 unit tests for BooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # qa_python
+1.  test_add_new_book_not_add_book
+тестируем органичение вводимого названия книги через параметриазцию
+2. test_add_new_book_add_two_books
+добавление2ух книг. 
+в тесте проверили метод add_new_book и get_books_genre
+3. test_add_new_book_add_this_book_empty_values
+тест на то что книга вообще добавилась с нужным именем и с пустым значением
+4. test_set_book_genre_success_add_genre
+тест на проверку того, что добавился к книге нужный жанр 
+5. test_get_books_genre_is_empty
+проверка что словарь пустой пока в него ничего не добавили
+6. test_get_books_with_specific_genre_have_book_with_genre
+тест добавился ли специфичный жанр
+7. test_get_books_for_children_not_have_specific_genre_in_list
+Книги с возрастным рейтингом отсутствуют в списке книг для детей.
+8. test_add_book_in_favorites_add_three_books
+добавляем 3 избранные книги 
+9. test_delete_book_from_favorites_not_success_del
+Неудачное удаление избранной книги
--- tests.py
+++ tests.py
@@ -1,24 +1,85 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+#тестируем органичение вводимого названия книги
+    @pytest.mark.parametrize('name',
+         [
+            '',
+            'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+         ]
+    )
+    def test_add_new_book_not_add_book(self, name):
         collector = BooksCollector()
 
-        # добавляем две книги
+        collector.add_new_book(name)
+        assert collector.get_books_genre() == {}
+
+    # тестируем add_two_books - добавление двух книг
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
+
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_this_book_empty_values(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('101 долматинец')
+        #проверяем что добавилась именно эта книга без жанра
+        assert collector.get_books_genre() == {'101 долматинец':''}
+
+    def test_set_book_genre_success_add_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Му-му')
+        collector.set_book_genre('Му-му', 'Ужасы')
+
+        assert collector.get_book_genre('Му-му') == 'Ужасы'
+
+
+    def test_get_books_genre_is_empty(self):
+        collector = BooksCollector()
+        assert collector.get_books_genre() == {}
+
+    def test_get_books_with_specific_genre_have_book_with_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Операция Ы')
+        collector.set_book_genre('Операция Ы', 'Комедии')
+        assert collector.get_books_with_specific_genre('Комедии') == ['Операция Ы']
+
+#проверяем что в детском списке нет книг с возрастным рейтингом
+    def test_get_books_for_children_not_have_specific_genre_in_list(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Операция Ы')
+        collector.set_book_genre('Операция Ы', 'Комедии')
+        collector.add_new_book('Том и Джерри')
+        collector.set_book_genre('Том и Джерри', 'Мультфильмы')
+        assert (collector.get_books_with_specific_genre('Ужасы') and collector.get_books_with_specific_genre('Детективы')) not in collector.get_books_for_children()
+
+        # добавляем 3 книги в Избранное
+    def test_add_book_in_favorites_add_three_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Операция Ы')
+        collector.add_new_book('Петр I')
+        collector.add_new_book('Мастер и Маргарита')
+        collector.add_book_in_favorites('Операция Ы')
+        collector.add_book_in_favorites('Петр I')
+        collector.add_book_in_favorites('Мастер и Маргарита')
+        assert len(collector.get_list_of_favorites_books()) == 3
+
+        # удаляем книгу из Избранного
+    def test_delete_book_from_favorites_not_success_del(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Операция Ы')
+        collector.add_book_in_favorites('Операция Ы')
+        collector.delete_book_from_favorites('Операция Ы ')
+        assert 'Операция Ы' in collector.get_list_of_favorites_books()
